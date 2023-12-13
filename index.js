394e24c9334c91d13cfe8c3c0c68ab7d8576211a
denny-svg <pknowledge@gmail.com>
2023-12-13 14:12:57 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return ['Antonia', 'Nuru'];  
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return  ['Amari', 'Mo'];  
+};
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (integer) {
+   return function (fare) {
+     return fare * integer;
+   };
+   };
+
+   const fareDoubler =createFareMultiplier(2); 
+   const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driverSelectors) {
+    return driverSelectors (drivers);   
+};
+
+
+
+   
+   
+
+
+
